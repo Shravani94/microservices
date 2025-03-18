b183a9f2f4038cffe5f349e2ba1b30a77782d8c0
Shravani94 <shravani.a@techouts.co.in>
2025-03-18 18:41:09 +0530

    hotel booking

---
+++ hotelBookings/src/test/java/com/hotelBookings/HotelBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelBookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
